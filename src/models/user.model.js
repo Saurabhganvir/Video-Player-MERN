e7d3fd2cbed00f25b65e62bb3e5758882514d906
Saurabh Ganvir <saurabhganvir14@gmail.com>
2025-07-09 03:48:03 +0530

    access tokens and refresh tokens

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -58,4 +58,34 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password);
 };
 
+//adding jwt tokens
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email:this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+//refresh token being added as a method
+userSchema.methods.generateRefreshToken =  function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 export const User = mongoose.model('User', userSchema);
